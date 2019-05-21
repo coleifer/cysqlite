cfca42125c4497f96b482729b56c6c88a1e15483
Charles Leifer <coleifer@gmail.com>
2019-05-21 10:57:42 -0500

    transactions, savepoints atomic.

--- src/cysqlite.pyx
+++ src/cysqlite.pyx
@@ -13,6 +13,7 @@ from cpython.unicode cimport PyUnicode_DecodeUTF8
 
 from collections import namedtuple
 import traceback
+import uuid
 
 from src.cysqlite cimport *
 
@@ -36,7 +37,15 @@ cdef raise_sqlite_error(sqlite3 *db):
     raise SqliteError(decode(errmsg))
 
 
-cdef class Connection(object):
+cdef class _callable_context_manager(object):
+    def __call__(self, fn):
+        def inner(*args, **kwargs):
+            with self:
+                return fn(*args, **kwargs)
+        return inner
+
+
+cdef class Connection(_callable_context_manager):
     cdef:
         sqlite3 *db
         public int cached_statements
@@ -50,6 +59,7 @@ cdef class Connection(object):
         # List of backup handles?
         dict stmt_available  # sql -> Statement.
         dict stmt_in_use  # id(stmt) -> Statement.
+        int _transaction_depth
 
     def __init__(self, database, flags=None, timeout=5000, vfs=None,
                  cached_statements=100):
@@ -61,6 +71,7 @@ cdef class Connection(object):
         self.db = NULL
         self.stmt_available = {}
         self.stmt_in_use = {}
+        self._transaction_depth = 0
 
     def __dealloc__(self):
         if self.db:
@@ -106,9 +117,15 @@ cdef class Connection(object):
 
         return True
 
+    def __enter__(self):
+        if not self.db:
+            self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
     cdef Statement prepare(self, sql, params=None):
-        # TODO: load from cache.
-        #cdef Statement st = Statement(self, sql)
         cdef Statement st = self.stmt_get(sql)
         st.bind(params)
         return st
@@ -168,6 +185,25 @@ cdef class Connection(object):
     def last_insert_rowid(self):
         return sqlite3_last_insert_rowid(self.db)
 
+    def transaction(self, lock=None):
+        return Transaction(self, lock)
+
+    def savepoint(self, sid=None):
+        return Savepoint(self, sid)
+
+    def atomic(self, lock=None):
+        return Atomic(self, lock)
+
+    def begin(self, lock=None):
+        lock = encode(lock or b'DEFERRED')
+        self.execute(b'BEGIN %s' % lock)
+
+    def commit(self):
+        self.execute(b'COMMIT')
+
+    def rollback(self):
+        self.execute(b'ROLLBACK')
+
 
 cdef int _exec_callback(void *data, int argc, char **argv, char **colnames) with gil:
     cdef:
@@ -191,6 +227,105 @@ cdef int _exec_callback(void *data, int argc, char **argv, char **colnames) with
         return SQLITE_ERROR
 
     return SQLITE_OK
+
+
+cdef class Transaction(_callable_context_manager):
+    cdef:
+        Connection conn
+        bytes lock
+
+    def __init__(self, Connection conn, lock=None):
+        self.conn = conn
+        self.lock = encode(lock or b'DEFERRED')
+
+    def _begin(self):
+        self.conn.execute(b'BEGIN %s' % self.lock)
+
+    def commit(self, begin=True):
+        self.conn.execute(b'COMMIT')
+        if begin: self._begin()
+
+    def rollback(self, begin=True):
+        self.conn.execute(b'ROLLBACK')
+        if begin: self._begin()
+
+    def __enter__(self):
+        if self.conn._transaction_depth < 1:
+            self._begin()
+        self.conn._transaction_depth += 1
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        is_bottom = self.conn._transaction_depth == 1
+        try:
+            if exc_type:
+                # If there are still more transactions on the stack, then we
+                # will begin a new transaction.
+                self.rollback(not is_bottom)
+            elif is_bottom:
+                try:
+                    self.commit(False)
+                except:
+                    self.rollback(False)
+        finally:
+            self.conn._transaction_depth -= 1
+
+
+cdef class Savepoint(_callable_context_manager):
+    cdef:
+        Connection conn
+        bytes quoted_sid
+        bytes sid
+
+    def __init__(self, Connection conn, sid=None):
+        self.conn = conn
+        self.sid = encode(sid or 's' + uuid.uuid4().hex)
+        self.quoted_sid = b'"%s"' % self.sid
+
+    def _begin(self):
+        self.conn.execute(b'SAVEPOINT %s;' % self.quoted_sid)
+
+    def commit(self, begin=True):
+        self.conn.execute(b'RELEASE SAVEPOINT %s;' % self.quoted_sid)
+        if begin: self._begin()
+
+    def rollback(self):
+        self.conn.execute(b'ROLLBACK TO SAVEPOINT %s;' % self.quoted_sid)
+
+    def __enter__(self):
+        self._begin()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            self.rollback()
+        else:
+            try:
+                self.commit(begin=False)
+            except:
+                self.rollback()
+                raise
+
+
+cdef class Atomic(_callable_context_manager):
+    cdef:
+        Connection conn
+        bytes lock
+        object txn
+
+    def __init__(self, Connection conn, lock=None):
+        self.conn = conn
+        self.lock = encode(lock or b'DEFERRED')
+
+    def __enter__(self):
+        if self.conn._transaction_depth == 0:
+            self.txn = self.conn.transaction(self.lock)
+        else:
+            self.txn = self.conn.savepoint()
+        return self.txn.__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return self.txn.__exit__(exc_type, exc_val, exc_tb)
 
 
 cdef class Statement(object):
@@ -297,7 +432,7 @@ cdef class Statement(object):
         if self.step_status == SQLITE_DONE:
             self.reset()
         elif self.step_status == SQLITE_ROW:
-            return iter(self)
+            return self
         else:
             raise_sqlite_error(self.conn.db)
 
