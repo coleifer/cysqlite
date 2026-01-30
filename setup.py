abbb3d5e7bd21c44c833dcfd4e2b4c516f3e0e5a
Charles Leifer <coleifer@gmail.com>
2026-01-30 14:26:32 -0600

    Modernize package scripts, also option for static linking.

--- setup.py
+++ setup.py
@@ -1,37 +1,56 @@
-import glob
+import os
+import sys
 import warnings
 
 from setuptools import setup
 from setuptools.extension import Extension
 try:
     from Cython.Build import cythonize
+    cython_installed = True
 except ImportError:
     cython_installed = False
-    warnings.warn('Cython not installed, using pre-generated C source file.')
-else:
-    cython_installed = True
-
 
 if cython_installed:
-    #python_sources = glob.glob('src/*.pyx')
-    python_sources = ['src/cysqlite.pyx']
+    sources = ['src/cysqlite.pyx']
 else:
-    #python_sources = glob.glob('src/*.c')
-    python_sources = ['src/cysqlite.c']
+    sources = ['src/cysqlite.c']
     cythonize = lambda obj: obj
 
-cysqlite_extension = Extension('cysqlite', libraries=['sqlite3'],
-                               sources=python_sources)
+if os.path.exists('src/sqlite3.c') and os.path.exists('src/sqlite3.h'):
+    sources.append('src/sqlite3.c')
+    include_dirs = ['src/']
+    libraries = []
+    define_macros = [
+        ('SQLITE_ALLOW_COVERING_INDEX_SCAN', 1),
+        ('SQLITE_ENABLE_FTS3', 1),
+        ('SQLITE_ENABLE_FTS3_PARENTHESIS', 1),
+        ('SQLITE_ENABLE_FTS4', 1),
+        ('SQLITE_ENABLE_FTS5', 1),
+        ('SQLITE_ENABLE_JSON1', 1),
+        ('SQLITE_ENABLE_LOAD_EXTENSION', 1),
+        ('SQLITE_ENABLE_MATH_FUNCTIONS', 1),
+        ('SQLITE_ENABLE_RTREE', 1),
+        ('SQLITE_ENABLE_STAT4', 1),
+        ('SQLITE_ENABLE_UPDATE_DELETE_LIMIT', 1),
+        ('SQLITE_SOUNDEX', 1),
+        ('SQLITE_USE_URI', 1),
+        ('SQLITE_TEMP_STORE', 3),
+        ('SQLITE_MAX_VARIABLE_NUMBER', 250000),
+        ('SQLITE_MAX_MMAP_SIZE', 2**40),
+    ]
+else:
+    include_dirs = []
+    libraries = ['sqlite3']
+    define_macros = []
 
-setup(
-    name='cysqlite',
-    version='0.1.0',
-    description='',
-    author='Charles Leifer',
-    author_email='',
-    url='https://github.com/coleifer',
-    license='MIT',
-    install_requires=['Cython'],
-    setup_requires=['cython'],
-    ext_modules=cythonize([cysqlite_extension])
-)
+compile_args = ['-O3', '-Wall'] if sys.platform != 'win32' else ['/O2']
+
+cysqlite_extension = Extension(
+    'cysqlite',
+    sources=sources,
+    include_dirs=include_dirs,
+    libraries=libraries,
+    define_macros=define_macros,
+    extra_compile_args=compile_args)
+
+setup(ext_modules=cythonize([cysqlite_extension]))
