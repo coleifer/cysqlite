69ceb3ba4e7d0c0e0b603fccd194ef486d34ea9c
Charles Leifer <coleifer@gmail.com>
2026-02-08 08:55:04 -0600

    Add small bench script to compare perf of hot areas.
    Right now looks like sqlite3 is faster at bind and column, but slower at
    iterating result sets.
    
    Will see if there are ways to optimize our stmt binding & reading
    columns off the stmt.

---
+++ scripts/bench.py
@@ -0,0 +1,64 @@
+import contextlib
+import time
+
+import sqlite3
+import cysqlite
+
+sq3_db = sqlite3.connect(':memory:')
+cy_db = cysqlite.connect(':memory:')
+
+@contextlib.contextmanager
+def measure(db, name):
+    s = time.perf_counter()
+    yield
+    e = time.perf_counter()
+    print('%0.2f - %s [%s]' % (e - s, name, db))
+
+def test_bind(db):
+    params = [
+        'a' * 256,
+        b'b' * 256,
+        1,
+        2.,
+        None] * 100
+    binds = ', '.join(['?'] * len(params))
+    sql = 'select %s' % binds
+
+    with measure(db, 'bind'):
+        for i in range(20000):
+            db.execute(sql, params)
+
+def test_column(db):
+    values = list(range(100))
+    cols = ', '.join(['col%d' % i for i in range(len(values))])
+    db.execute('create table k (%s)' % cols)
+
+    binds = ', '.join(['?'] * len(values))
+    db.execute('insert into k values (%s)' % binds, values)
+
+    with measure(db, 'column'):
+        for i in range(20000):
+            db.execute('select * from k').fetchone()
+
+    db.execute('drop table k')
+
+
+def test_iterate(db):
+    db.execute('create table k (id integer not null primary key, data text)')
+    db.executemany('insert into k (data) values (?)',
+                   [('k%06d' % i,) for i in range(10000)])
+
+    with measure(db, 'iterate'):
+        for i in range(1000):
+            db.execute('select * from k').fetchall()
+
+    db.execute('drop table k')
+
+test_bind(sq3_db)
+test_bind(cy_db)
+
+test_column(sq3_db)
+test_column(cy_db)
+
+test_iterate(sq3_db)
+test_iterate(cy_db)
