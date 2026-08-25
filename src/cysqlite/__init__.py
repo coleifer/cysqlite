5e1848469f20061d9746f96479541e225f653e4a
Charles Leifer <coleifer@gmail.com>
2026-08-25 15:18:25 -0500

    0.3.7

--- src/cysqlite/__init__.py
+++ src/cysqlite/__init__.py
@@ -23,7 +23,7 @@ from cysqlite._constants import *
 from cysqlite.exceptions import *
 
 
-version = __version__ = '0.3.6'
+version = __version__ = '0.3.7'
 version_info = tuple(int(i) for i in version.split('.'))
 
 # DB-API 2.0 module attributes.
